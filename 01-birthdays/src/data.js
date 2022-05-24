4d87efbb405a0af48bafdc13c0a306d87c0df700
Digiraj Solutions <digiraj.in@gmail.com>
2022-05-24 11:58:34 +0530

    birthdays comppleted

--- 01-birthdays/src/data.js
+++ 01-birthdays/src/data.js
@@ -1,37 +1,39 @@
-export default [
+let data = [
   {
     id: 1,
-    name: 'Bertie Yates',
+    name: 'Anushka Sharma',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-2_ipcjws.jpg',
   },
   {
     id: 2,
-    name: 'Hester Hogan',
+    name: 'Katrina Kaif',
     age: 32,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1595959131/person-3_rxtqvi.jpg',
   },
   {
     id: 3,
-    name: 'Larry Little',
+    name: 'Vicky Kaushal',
     age: 36,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
   },
   {
     id: 4,
-    name: 'Sean Walsh',
+    name: 'Aditya Kashyap',
     age: 34,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
   },
   {
     id: 5,
-    name: 'Lola Gardner',
+    name: 'Mira Rajput',
     age: 29,
     image:
       'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
   },
-];
+]
+
+export default data
