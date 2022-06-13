4a206a738a59dd86d74ca7692cd0f07094ce19c1
Digiraj Solutions <digiraj.in@gmail.com>
2022-06-13 19:56:07 +0530

    Cocktails API - Initial setup

---
+++ 06-cocktails-api/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import SingleCocktail from './pages/SingleCocktail'
+import Error from './pages/Error'
+import Navbar from './components/Navbar'
+function App() {
+  return (
+    <>
+      <Router></Router>
+    </>
+  )
+}
+
+export default App
